bea6abe07e0880fe9bedc250e92c0006f58cbf18
leesangeok <wyzmqkr11@naver.com>
2024-12-05 11:35:50 +0900

    beforeEach r

--- src/test/java/com/ll/simpleDb/SimpleDbTest.java
+++ src/test/java/com/ll/simpleDb/SimpleDbTest.java
@@ -1,37 +1,29 @@
 package com.ll.simpleDb;
 
-//import com.ll.Article;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.*;
 
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.IntStream;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @TestMethodOrder(MethodOrderer.MethodName.class)
 public class SimpleDbTest {
     private static SimpleDb simpleDb;
-
     @BeforeAll
     public static void beforeAll() {
         simpleDb = new SimpleDb("localhost", "root", "lldj123414", "simpleDb__test");
-
         createArticleTable();
+    }
 
+    @BeforeEach
+    public void beforeEach() {
+        truncateArticleTable();
+        makeArticleTestData();
     }
 
     private static void createArticleTable() {
         simpleDb.run("DROP TABLE IF EXISTS article");
-
         simpleDb.run("""
                 CREATE TABLE article (
                     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -44,4 +36,34 @@ public class SimpleDbTest {
                 )
                 """);
     }
+
+    private void makeArticleTestData() {
+        IntStream.rangeClosed(1, 6).forEach(no -> {
+            boolean isBlind = no > 3;
+            String title = "제목%d".formatted(no);
+            String body = "내용%d".formatted(no);
+
+            simpleDb.run("""
+                    INSERT INTO article
+                    SET createdDate = NOW(),
+                    modifiedDate = NOW(),
+                    title = ?,
+                    `body` = ?,
+                    isBlind = ?
+                    """, title, body, isBlind);
+        });
+    }
+
+    private void truncateArticleTable() {
+        simpleDb.run("TRUNCATE article");
+    }
+
+    @Test
+    @DisplayName("데이터베이스 연결 테스트")
+    public void t000() {
+
+    }
+
+
+
 }
