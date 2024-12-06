1b50fb5947a4790627e296a902ed80487cfecb62
leesangeok <wyzmqkr11@naver.com>
2024-12-06 11:42:25 +0900

    insert

--- src/main/java/com/ll/simpleDb/SimpleDb.java
+++ src/main/java/com/ll/simpleDb/SimpleDb.java
@@ -24,7 +24,7 @@ public class SimpleDb {
             return; // 이미 연결되어 있으면 아무 작업도 하지 않음
         }
 
-        String url = String.format("jdbc:mysql://%s:3307/%s?useSSL=false", host, dbName);
+        String url = String.format("jdbc:mysql://%s:3307/%s?useSSL=false&allowPublicKeyRetrieval=true", host, dbName);
         try {
             connection = DriverManager.getConnection(url, username, password);
         } catch (SQLException e) {
@@ -45,142 +45,141 @@ public class SimpleDb {
         }
     }
 
+
+    public Sql genSql() {
+        return new Sql(this);
+    }
     // PreparedStatement 파라미터 바인딩 분리
     private void bindParameters(PreparedStatement preparedStatement, Object... params) throws SQLException {
         for (int i = 0; i < params.length; i++) {
             preparedStatement.setObject(i + 1, params[i]);
         }
     }
-
-    public Sql genSql() {
-        return new Sql(this);
+    // SQL 실행 메서드
+    private Map<String, Object> parseResultSetToMap(ResultSet resultSet) throws SQLException {
+        ResultSetMetaData metaData = resultSet.getMetaData();
+        int columnCount = metaData.getColumnCount();
+        Map<String, Object> row = new LinkedHashMap<>();
+        for (int i = 1; i <= columnCount; i++) {
+            String columnName = metaData.getColumnLabel(i);
+            Object value;
+            switch (metaData.getColumnType(i)) {
+                case Types.BIGINT:
+                    value = resultSet.getLong(columnName);
+                    break;
+                case Types.TIMESTAMP:
+                    Timestamp timestamp = resultSet.getTimestamp(columnName);
+                    value = (timestamp != null) ? timestamp.toLocalDateTime() : null;
+                    break;
+                case Types.BOOLEAN:
+                    value = resultSet.getBoolean(columnName);
+                    break;
+                default:
+                    value = resultSet.getObject(columnName);
+                    break;
+            }
+            row.put(columnName, value);
+        }
+        return row;
     }
 
 
+    private <T> T parseResultSet(ResultSet resultSet, Class cls) throws SQLException {
+        if (cls == String.class) {
+            resultSet.next();
+            return (T) resultSet.getString(1);
+        } else if (cls == List.class) {
+            List<Map<String, Object>> rows = new ArrayList<>();
+            while (resultSet.next()) {
+                rows.add(parseResultSetToMap(resultSet));
+            }
+            return (T) rows;
+        } else if (cls == Map.class) {
+            resultSet.next();
+            return (T) parseResultSetToMap(resultSet);
+        } else if (cls == LocalDateTime.class) {
+            resultSet.next();
+            return (T) resultSet.getTimestamp(1).toLocalDateTime();
+        } else if (cls == Long.class) {
+            resultSet.next();
+            return (T) (Long) resultSet.getLong(1);
+        } else if (cls == Boolean.class) {
+            resultSet.next();
+            return (T) (Boolean) resultSet.getBoolean(1);
+        }
+        throw new IllegalArgumentException("Unsupported class type: " + cls);
+    }
 
-    // SQL 실행 메서드
-    public <T> T _run(String sql, Class cls, Object... params) {
-        connect(); // 연결 초기화
 
-        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-            bindParameters(preparedStatement, params); // 파라미터 바인딩 로직 분리
+    private <T> T _run(String sql, Class cls, Object... params) {
+        connect();
+        if( sql.startsWith("INSERT")){
+            try (PreparedStatement preparedStatement = connection.prepareStatement(sql,PreparedStatement.RETURN_GENERATED_KEYS)){
+                bindParameters(preparedStatement,params);
 
-            if(sql.startsWith("SELECT")) {
-                ResultSet resultSet = preparedStatement.executeQuery();
+                int affectedRows = preparedStatement.executeUpdate();
 
-                if ( cls == String.class) {
-                    resultSet.next();
-                    return  (T) resultSet.getString(1);
-                }else if (cls == List.class) {
-                    List<Map<String, Object>> rows = new ArrayList<>();
-
-                    ResultSetMetaData metaData = resultSet.getMetaData();
-                    int columnCount = metaData.getColumnCount();
-
-                    while (resultSet.next()) {
-                        Map<String, Object> row = new LinkedHashMap<>();
-
-                        for (int i = 1; i <= columnCount; i++) {
-                            String columnName = metaData.getColumnLabel(i);
-                            Object value;
-
-                            switch (metaData.getColumnType(i)) {
-                                case Types.BIGINT:
-                                    value = resultSet.getLong(columnName);
-                                    break;
-                                case Types.TIMESTAMP:
-                                    Timestamp timestamp = resultSet.getTimestamp(columnName);
-                                    value = (timestamp != null) ? timestamp.toLocalDateTime() : null;
-                                    break;
-                                case Types.BOOLEAN:
-                                    value = resultSet.getBoolean(columnName);
-                                    break;
-                                default:
-                                    value = resultSet.getObject(columnName);
-                                    break;
-                            }
-
-                            row.put(columnName, value);
-                        }
-
-                        rows.add(row);
-                    }
-
-                    return (T) rows;
-                } else if( cls == Map.class){
-                    resultSet.next();
-                    Map<String, Object> row = new LinkedHashMap<>();
-                    ResultSetMetaData metaData = resultSet.getMetaData();
-                    int columnCount = metaData.getColumnCount();
-
-                    for (int i = 1; i <= columnCount; i++) {
-                        String columnName = metaData.getColumnLabel(i);
-                        Object value;
-
-                        switch (metaData.getColumnType(i)) {
-                            case Types.BIGINT:
-                                value = resultSet.getLong(columnName);
-                                break;
-                            case Types.TIMESTAMP:
-                                Timestamp timestamp = resultSet.getTimestamp(columnName);
-                                value = (timestamp != null) ? timestamp.toLocalDateTime() : null;
-                                break;
-                            case Types.BOOLEAN:
-                                value = resultSet.getBoolean(columnName);
-                                break;
-                            default:
-                                value = resultSet.getObject(columnName);
-                                break;
-                        }
-
-                        row.put(columnName, value);
-                    }
-                    return (T) row;
-                } else if( cls == LocalDateTime.class){
-                    resultSet.next();
-                    return  (T) resultSet.getTimestamp(1).toLocalDateTime();
-                }else if( cls == Long.class){
-                    resultSet.next();
-                    return  (T) (Long) resultSet.getLong(1);
-                } else if( cls == Boolean.class){
-                    resultSet.next();
-                    return  (T) (Boolean) resultSet.getBoolean(1);
+                if ( cls == Integer.class){
+                    return (T) (Integer) affectedRows;
                 }
+                ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
+                if(generatedKeys.next()){
+                    return (T) (Long) generatedKeys.getLong(1);
+                }
+            } catch (SQLException e){
+                throw new RuntimeException("Failed to execute SQL: " + sql + ". Error: " + e.getMessage(), e);
+            }
+        }
+            try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            bindParameters(preparedStatement, params);
+
+           if (sql.startsWith("SELECT")) {
+                ResultSet resultSet = preparedStatement.executeQuery();
+                return parseResultSet(resultSet, cls);
             }
             return (T) (Integer) preparedStatement.executeUpdate();
-
         } catch (SQLException e) {
             throw new RuntimeException("Failed to execute SQL: " + sql + ". Error: " + e.getMessage(), e);
         }
     }
 
-    public int run(String sql, Object... params){
-        return (int) _run(sql, Integer.class, params);
+    public int run(String sql, Object... params) {
+        return _run(sql, Integer.class, params);
     }
 
-
-    public boolean selectBoolean(String sql) {
-        return  (boolean) _run(sql,Boolean.class);
+    public boolean selectBoolean(String sql,Object... params) {
+        return _run(sql, Boolean.class);
     }
 
-    public String selectString(String sql) {
-        return  (String) _run(sql,String.class);
+    public String selectString(String sql,Object... params) {
+        return _run(sql, String.class);
     }
 
-    public long selectLong(String sql) {
-        return (long) _run(sql, Long.class);
+    public long selectLong(String sql,Object... params) {
+        return _run(sql, Long.class);
     }
 
-    public LocalDateTime selectDatetime(String sql){
-        return _run(sql,LocalDateTime.class);
+    public LocalDateTime selectDatetime(String sql,Object... params) {
+        return _run(sql, LocalDateTime.class);
     }
 
-    public Map<String, Object> selectMap(String sql) {
+    public Map<String, Object> selectRow(String sql,Object... params) {
         return _run(sql, Map.class);
     }
 
-    public List<Map<String, Object>> selectRows(String sql) {
+    public List<Map<String, Object>> selectRows(String sql,Object... params) {
         return _run(sql, List.class);
     }
-}
 
+    public int delete(String sql, Object... params) {
+        return _run(sql, Integer.class, params);
+    }
+
+    public int update(String sql, Object[] params) {
+        return _run(sql, Integer.class, params);
+    }
+
+    public long insert(String sql, Object[] params) {
+        return _run(sql, Long.class, params);
+    }
+}
--- src/main/java/com/ll/simpleDb/Sql.java
+++ src/main/java/com/ll/simpleDb/Sql.java
@@ -9,51 +9,62 @@ import java.util.Map;
 
 public class Sql {
     private final SimpleDb simpleDb;
-    private  StringBuilder sqlFormat;
+    private  final StringBuilder sqlFormat;
+    private  final List<Object> params;
 
     public  Sql(SimpleDb simpleDb) {
         this.simpleDb = simpleDb;
+        this.params = new ArrayList<>();
         this.sqlFormat = new StringBuilder();
     }
-    public Sql append(String sqlBit, Object... param) {
+    public Sql append(String sqlBit, Object... params) {
         this.sqlFormat.append(" " + sqlBit);
+
+        // 필드 params 가 아닌, 메서드 파라미터 param을 순회해야 함
+        for (Object param : params) {
+            this.params.add(param);
+        }
+
         return this;
+    }
+    private  String toSql(){
+        return sqlFormat.toString().trim();
     }
 
     public long insert() {
-        return 1;
+        return simpleDb.insert(toSql(),params.toArray());
     }
 
     public int update() {
-        return 3;
+        return simpleDb.update(toSql(),params.toArray());
     }
 
     public int delete() {
-        return 2;
+        return simpleDb.delete(toSql(),params.toArray());
     }
 
     public List<Map<String, Object>> selectRows() {
-        return simpleDb.selectRows(sqlFormat.toString().trim());
+        return simpleDb.selectRows(toSql().trim(),params.toArray());
     }
 
     public Map<String, Object> selectRow() {
-        return simpleDb.selectMap(sqlFormat.toString().trim());
+        return simpleDb.selectRow(toSql().trim(),params.toArray());
     }
 
     public LocalDateTime selectDatetime() {
-        return simpleDb.selectDatetime(sqlFormat.toString().trim());
+        return simpleDb.selectDatetime(toSql(),params.toArray());
     }
 
 
     public long selectLong() {
-        return simpleDb.selectLong(sqlFormat.toString().trim());
+        return simpleDb.selectLong(toSql(),params.toArray());
     }
 
     public String selectString() {
-        return simpleDb.selectString(sqlFormat.toString().trim());
+        return simpleDb.selectString(toSql(),params.toArray());
     }
 
     public boolean selectBoolean() {
-        return simpleDb.selectBoolean(sqlFormat.toString().trim());
+        return simpleDb.selectBoolean(toSql(),params.toArray());
     }
 }
